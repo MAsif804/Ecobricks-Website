bc4ff8e3d131b37a699b91d96d02c56c04d1f2ad
MAsif804 <asif.pkml@gmail.com>
2025-03-05 19:40:33 +0500

    same changes

--- src/component/Navbar_pp/Navbar_pp.jsx
+++ src/component/Navbar_pp/Navbar_pp.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import './Navbar_pp.css';
-import {ecobricksLogo2, whatappsImg} from "../../assets/Index";
+import {ecobricksLogo, whatappsImg} from "../../assets/Index";
 const Navbar = () => {
   return (
     <nav id-='navbar_pp'>
             <div className="logo-ecobricks-pp" onClick={() => window.location.href = "#"} >
-              <img src={ecobricksLogo2} alt="" />
+              <img src={ecobricksLogo} alt="" />
               <p>Ecobricks</p>  
             </div>
             <div className="navbar-items-pp">
